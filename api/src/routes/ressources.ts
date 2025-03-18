022e8faaf39577b22500868a72c7c0e5ac9b1394
Fadi <fadibouchnak1@gmail.com>
2025-03-18 19:59:57 +0100

    feat(routes): created deleteRessourceById route [DELETE]

--- api/src/routes/ressources.ts
+++ api/src/routes/ressources.ts
@@ -12,41 +12,60 @@ router.get("/", async (req, res) => {
     if (ressources && ressources.length > 0) res.status(200).json(ressources);
     else res.status(404).json({ message: "No ressources found." });
   } catch (e) {
-    // dev
-    res.status(500).json(e);
-    // prod
-    // res.status(500).json("Internal server error. Please contact an administrateur or IT service.");
-  }
-});
-
-router.post<{}, any, RessourceEntity>("/", async (req, res) => {
-  const { title, description, categoryId } = req.body;
-
-  try {
-    const category = await prisma.category.findFirst({
-      where: { id: categoryId },
-    });
-
-    if (!category) {
-      res.status(400).json({ error: "Category not found" });
-      return;
-    }
-
-    const newRessource = await prisma.ressource.create({
-      data: {
-        title,
-        description,
-        category: {
-          connect: { id: categoryId },
-        },
-      },
-    });
-
-    res.status(201).json({ data: newRessource });
-  } catch (e) {
     console.error(e);
     res.status(500).json({ error: "Internal server error", details: e });
   }
 });
+
+router.post<{}, any, RessourceEntity>(
+  "/",
+  async (req: TypedRequestBody<RessourceCreateBody>, res) => {
+    const { title, description, categoryId } = req.body;
+
+    try {
+      const category = await prisma.category.findFirst({
+        where: { id: categoryId },
+      });
+
+      if (!category) {
+        res.status(400).json({ error: "Category not found" });
+        return;
+      }
+
+      const newRessource = await prisma.ressource.create({
+        data: {
+          title,
+          description,
+          category: {
+            connect: { id: categoryId },
+          },
+        },
+      });
+
+      res.status(201).json({ data: newRessource });
+    } catch (e) {
+      console.error(e);
+      res.status(500).json({ error: "Internal server error", details: e });
+    }
+  }
+);
+
+router.delete(
+  "/:ressourceId",
+  async (req: TypedRequestBody<RessourceEntity>, res) => {
+    try {
+      const ressourceId = req.params.ressourceId;
+
+      await prisma.ressource.delete({ where: { id: parseInt(ressourceId) } });
+
+      res
+        .status(204)
+        .json(`La ressource (id:${ressourceId}) a été supprimé avec succès.`);
+    } catch (e) {
+      console.error(e);
+      res.status(500).json({ error: "Internal server error", details: e });
+    }
+  }
+);
 
 export default router;
