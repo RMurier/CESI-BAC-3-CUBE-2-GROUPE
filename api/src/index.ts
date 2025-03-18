9c8b669ceb4f06179d7ae88580646f488141c8c7
Fadi <fadibouchnak1@gmail.com>
2025-03-18 18:21:37 +0100

    add(Prisma): created initial prisma schema and migration

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,21 +1,41 @@
-import * as dotenv from "dotenv";
-import express from "express";
-import cors from "cors";
+import "dotenv/config";
+import express, { json } from "express";
+import { rateLimit } from "express-rate-limit";
+// import users from "./routes/users";
+// import articles from "./routes/articles";
+import prisma from "./utils/database";
+// import auth from "./routes/auth";
 import helmet from "helmet";
+import "./utils/passport";
+import passport from "passport";
+import { clerkMiddleware } from "@clerk/express";
 
-if (!process.env.PORT) {
+async function main() {
+  const app = express();
+  const port = 3000;
+
+  app.use(passport.initialize());
+  app.use(express.json());
+  app.use(helmet());
+  // app.use('/users', users);
+  // app.use('/articles', articles);
+  app.use(express.urlencoded({ extended: false }));
+  app.use(clerkMiddleware());
+
+  // app.use("/users", passport.authenticate("jwt", { session: false }), users);
+  // app.use('/auth', auth);
+
+  app.listen(port, () => {
+    console.log(`App running and listening on http://127.0.0.1:${port}`);
+  });
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
     process.exit(1);
- }
- 
- const PORT: number = parseInt(process.env.PORT as string, 10);
- 
- const app = express();
- app.use(helmet());
-app.use(cors());
-app.use(express.json());
-
-dotenv.config();
-
-app.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}`);
   });
