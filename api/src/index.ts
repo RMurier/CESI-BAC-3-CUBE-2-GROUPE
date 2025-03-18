bb07c297a6aa844638218bea38549b7774a87185
RMurier <murierromain@gmail.com>
2025-03-18 17:15:35 +0100

    first commit

---
+++ api/src/index.ts
@@ -0,0 +1,21 @@
+import * as dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+
+if (!process.env.PORT) {
+    process.exit(1);
+ }
+ 
+ const PORT: number = parseInt(process.env.PORT as string, 10);
+ 
+ const app = express();
+ app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+dotenv.config();
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+  });
