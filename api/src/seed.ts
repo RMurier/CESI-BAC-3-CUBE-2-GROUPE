212c0257fd92b2b717e093f12e7875d75e9e3efe
Fadi <fadibouchnak1@gmail.com>
2025-04-14 15:47:12 +0200

    add(db): new static data in seed.ts and conf for "npx prisma db seed" command

---
+++ api/src/seed.ts
@@ -0,0 +1,30 @@
+import prisma from "./utils/database";
+
+async function main() {
+  await prisma.category.create({
+    data: {
+      name: "Divers",
+      description: "Contient des ressources diverses.",
+    },
+  });
+  await prisma.role.createMany({
+    data: [
+      { name: "Citoyen" },
+      { name: "Modérateur" },
+      { name: "Admin" },
+      { name: "Super-Admin" },
+    ],
+  });
+  await prisma.ressourceType.createMany({
+    data: [{ name: "Public" }, { name: "Private" }, { name: "Shared" }],
+  });
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
