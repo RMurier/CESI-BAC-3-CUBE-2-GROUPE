3bda855ce958a2d8e569fcb151f1461b268db2d5
RMurier <murierromain@gmail.com>
2025-07-02 22:53:37 +0200

    fix

--- api/prisma/seed.ts
+++ api/prisma/seed.ts
@@ -1,11 +1,29 @@
 import prisma from "../src/utils/database";
 
 async function main() {
-  await prisma.category.create({
-    data: {
+  await prisma.category.createMany({
+    data: [
+      {
       name: "Divers",
       description: "Contient des ressources diverses.",
     },
+    {
+      name: "family",
+      description: "Ressources liées à la famille.",
+    },
+    {
+      name: "couple",
+      description: "Ressources liées au couple.",
+    },
+    {
+      name: "friends",
+      description: "Ressources liées aux amis.",
+    },
+    {
+      name: "work",
+      description: "Ressources liées au travail.",
+    }
+  ],
   });
   await prisma.role.createMany({
     data: [
